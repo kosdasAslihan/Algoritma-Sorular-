982caad2a59ca746b0bec13829e5f1ddf62135b5
kosdasAslıhan <kosdasaslihan@gmail.com>
2023-11-29 12:39:33 +0300

    calismalarim

---
+++ src/AbstractClasslar/Daire.java
@@ -0,0 +1,18 @@
+package AbstractClasslar;
+
+public class Daire extends Sekil {
+
+    private int yariCap;
+
+    public Daire(String isim,int yariCap) {
+        super(isim);
+        this.yariCap = yariCap;
+    }
+
+    @Override
+    void alan_hesapla() {
+
+        System.out.println(getIsim() + " alanı " + (Math.PI*yariCap*yariCap) + " dir. ");
+
+    }
+}
