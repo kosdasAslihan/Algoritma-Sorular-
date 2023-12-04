c7bf7f090b8b312781f92091f7ede31c68545d21
kosdasAslıhan <kosdasaslihan@gmail.com>
2023-12-04 14:26:35 +0300

    calismalarim

---
+++ src/LokalInnerClasslar/Main.java
@@ -0,0 +1,24 @@
+package LokalInnerClasslar;
+
+import StaticInnerClasslar.Matematik;
+
+public class Main {
+    public static void main(String[] args) {
+
+        class Alan {
+            public void daire_alan(int yaricap) {
+                System.out.println("Dairenin Alanı: " + (yaricap*yaricap*Math.PI));
+            }
+        }
+
+        Alan alan = new Alan();
+        Alan alan2 = new Alan();
+        alan.daire_alan(10);
+        alan2.daire_alan(3);
+    }
+
+    public static void deneme() {
+        //Alan alan1 = new Alan(); lokal inner class sadece yukarıdaki metoda özgü, bu yüzden buna başka metodlardan erişemiyoruz.
+
+    }
+}
